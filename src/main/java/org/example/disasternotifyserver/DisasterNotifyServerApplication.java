1fd67564caeb96fa79c30b46f05c079516e88cc6
Yarism1234 <leehappy201@gmail.com>
2024-09-15 15:48:10 +0900

    start

---
+++ src/main/java/org/example/disasternotifyserver/DisasterNotifyServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.disasternotifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterNotifyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterNotifyServerApplication.class, args);
+	}
+
+}

3a01be2077826d5dbbadc98f83af2e3efad4baf5
Yarism1234 <leehappy201@gmail.com>
2024-09-20 14:28:46 +0900

    1.start 2.로그인 회원가입 jwt 구현

--- src/main/java/org/example/disasternotifyserver/test/CrawlingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.disasternotifyserver.test;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CrawlingRepository extends MongoRepository<CrawlingInfo, String> {
-    CrawlingInfo findCrawlingInfoByName(String name);
-}
--- src/main/java/org/example/disasternotifyserver/test/StatusEnum.java
+++
@@ -1,7 +0,0 @@
-package org.example.disasternotifyserver.test;
-
-public enum StatusEnum {
-    OK, // 성공
-    ERROR // 에러
-    // 추가적인 상태 코드 정의 가능
-}
